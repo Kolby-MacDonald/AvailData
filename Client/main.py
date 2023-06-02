4b4085f5875ac1837b574ff95365cc0fa391db1c
Kolby MacDonald <115326488+Kolby-MacDonald@users.noreply.github.com>
2023-06-02 15:28:11 -0600

    Socket System Attempt

--- Client/main.py
+++ Client/main.py
@@ -8,35 +8,52 @@ from PyQt5.uic import loadUi
 from os import getenv
 from dotenv import load_dotenv
 
+# Define our client.
 CLIENT = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
+# Class to controll the login page functionality.
 class LoginPage(QDialog):
+    
+    # Initialize the attributes of the login parent class.
     def __init__(self):
         super(LoginPage,self).__init__()
-        #loadUi(r'C:\Users\deadb\Documents\GitHub\AvailData\login_page\login_page.ui',self)
-        loadUi(r'C:\Users\deadb\Documents\GitHub\AvailData-Public\Client\pages\login_page.ui',self)
-        self.login_button.clicked.connect(self.login_function)
-        self.linkedin_button.clicked.connect(self.open_linkedin)
+        loadUi(r'Client\pages\login_page.ui', self) # Load the login page UI after class is called.
+        self.login_button.clicked.connect(self.login_function) # Define parameters for login buttons when clicked.
+        self.linkedin_button.clicked.connect(self.open_linkedin) # Define parameters for linkedin button when clicked.
 
 
+    # The user clicked the sign in button.
     def login_function(self):
-        global CLIENT                                  
-        username = str(self.username_line_edit.text())
-        password = str(self.password_line_edit.text())
-        enc_password = hashlib.sha256(password.encode()).hexdigest()
+
+        # Get required data.
+        global CLIENT # Get our global Client definition.                         
+        username = str(self.username_line_edit.text()) # Take the input username from the respective linedit.
+        password = str(self.password_line_edit.text()) # Take the input password from the respective linedit.
+        enc_password = hashlib.sha256(password.encode()).hexdigest() #SHA256 Hash the password before sending.
+        
+        # Clear the linedit fields.
         self.username_line_edit.setText("")
         self.password_line_edit.setText("")
 
+        # Send the data to the server and wait for a response.
         if username != "" and password !="":
+
+            #LOCALLY ENCRYPT THE DATA HERE
+
+            #Encode and send the data
             CLIENT.send(username.encode())
             CLIENT.send(enc_password.encode())
 
+            # Wait for a response.
             response = CLIENT.recv(1024).decode()
-            if response == "True":
-                self.open_user_page()
+
+            # Response based functionality.
+            # If somehow intercepted and bypassed by reverse engineering, the user access controll will prevent user data leaks.
+            if response == "True": # If the server has the requested user in the specific table.
+                self.open_user_page() # Open the main application window.
                 print("Success")
-            else:
-                CLIENT.shutdown(socket.SHUT_RDWR)
+            else: # If the server does not have the requested data.
+                CLIENT.shutdown(socket.SHUT_RDWR) # Restart the socket.
                 CLIENT.close()
                 CLIENT = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
                 CLIENT.connect((getenv("pub_Ip"), int(getenv("pub_port"))))
@@ -44,32 +61,47 @@ class LoginPage(QDialog):
         else:
             print("Verification failed! (Local)")
 
-
+    # Function to open the users web browser to creators LinkedIn.
     def open_linkedin(self):
         webbrowser.open('www.linkedin.com/in/kolby-macdonald')
 
+    # Function to send the user to the main application window.
     def open_user_page(self):
-        userwindow = UserPage()
-        widget.removeWidget(loginwindow)
-        widget.addWidget(userwindow)
-        #widget.setCurrentIndex(widget.currentIndex()+1) 
+        userwindow = UserPage() # Define the users main application windows existence.
+        widget.removeWidget(loginwindow) # Remove the login widget.
+        widget.addWidget(userwindow) # Add the users main application window.
 
+# The main application window where users will interact with their priveledge data.
 class UserPage(QDialog):
+
+    # Initialize the attributes of the userpage parent class.
     def __init__(self):
         super(UserPage, self).__init__()
-        loadUi(r'C:\Users\deadb\Documents\GitHub\AvailData-Public\Client\pages\user_page_test.ui',self)
+        loadUi(r'Client\pages\user_page_test.ui', self) # Load the corresponding ui.
+
+    #send request udpate_available_tables
+    
+    #send request update_results_selection
+
+    #send request load_table_data
+
+    #format table_data into a pd
+
+    #send the table_data into 
 
 
-def env_configure():
-    load_dotenv()
 
 
-env_configure()
-CLIENT.connect((getenv("pub_Ip"), int(getenv("pub_port"))))
+# Main Script:
 
+# Pre application requirements
+load_dotenv() # Load the environment variables.
+CLIENT.connect((getenv("pub_Ip"), int(getenv("pub_port")))) # Connect to the client
+
+# Application startup requirements and control.
 app=QApplication(sys.argv)
 widget=QtWidgets.QStackedWidget()
-loginwindow = LoginPage()
+loginwindow = LoginPage() # Define the existance of the login application.
 widget.addWidget(loginwindow)
 widget.resize(1080,720)
 widget.setMaximumWidth(1920)
--- Server/server.py
+++ Server/server.py
@@ -24,22 +24,60 @@ def handle_connection(c):
         f'''SELECT * FROM {os.getenv("db_table_name")} where username = %s and password = %s''',
         (credentials[0].decode(), credentials[1].decode())
         )
+    
+    try:
+        results = cursor.fetchone()
+        user_id = results[0]
+        user_name = results[1]
+        user_pass = results[2]
+        user_table_access = results[3]
+        print(f'user_id = {user_id}')
+        print(f'user_name = {user_name}')
+        print(f'user_table_access = {user_table_access}')
+        print("Inputed username = " + credentials[0].decode())
+    except:
+        pass
 
-    if cursor.fetchall():
+    if results:
         c.send("True".encode())
-        print(f"{credentials[0].decode()} has accessed the database.")
-        #Services go here
+        print(f"{user_name} has accessed the database.")
+        handle_requests(c)
     else:
         c.send("False".encode())
         print(f"{credentials[0].decode()} has failed to access the database.")
 
+    
 
-def env_configure():
-    load_dotenv()
+def handle_requests(c):
+    #Structure of requests:
+    #str(list(
+    # 0: request type
+    # 1: parameters list ()
+    # ))
 
+    #Structure of response is dependant on request
+
+    request_type = c.recv(1024)
+
+    if request_type[0] == "update_available_tables":
+        #get available tables from user table
+
+        #if not = "all" then return a string of specific tables
+
+    
+
+        #format into a list
+
+        #
+        pass
+    elif request_type[0] == "update_results_selection":
+        
+        pass
+
+       
 
 def main():
-    env_configure()
+    load_dotenv()
 
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     server_socket.bind((os.getenv("pub_Ip"), int(os.getenv("pub_port"))))
@@ -48,6 +86,8 @@ def main():
 
     while True:
         client, addr = server_socket.accept()
+        print(f"incoming connection by: {addr}")
         threading.Thread(target=handle_connection, args=(client,)).start()
+
 
 main()
