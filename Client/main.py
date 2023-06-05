3a6507d3b4190767b66b9535b8f8112c24cb6dc0
Kolby MacDonald <115326488+Kolby-MacDonald@users.noreply.github.com>
2023-06-04 23:45:12 -0600

    Signup Page Added
    Unimplemented functionality beyond gui, however the signup page will hold its place functionally for now.

--- Client/main.py
+++ Client/main.py
@@ -20,6 +20,7 @@ class LoginPage(QDialog):
         super(LoginPage,self).__init__()
         loadUi(r'Client\pages\login_page.ui', self) # Load the login page UI after class is called.
         self.login_button.clicked.connect(self.login_function) # Define parameters for login buttons when clicked.
+        self.signup_button.clicked.connect(self.open_signup_page) # Define parameters for signup page.
         self.linkedin_button.clicked.connect(self.open_linkedin) # Define parameters for linkedin button when clicked.
 
 
@@ -72,11 +73,39 @@ class LoginPage(QDialog):
     def open_linkedin(self):
         webbrowser.open('www.linkedin.com/in/kolby-macdonald')
 
+    # Function to open the signup page (currently inactive)
+    def open_signup_page(self):
+        widget.removeWidget(login_window) # Remove the login widget.
+        widget.addWidget(signup_window) # Add the users signup application window.
+
     # Function to send the user to the main application window.
     def open_user_page(self):
-        userwindow = UserPage() # Define the users main application windows existence.
-        widget.removeWidget(loginwindow) # Remove the login widget.
-        widget.addWidget(userwindow) # Add the users main application window.
+        user_window = UserPage() # Define the users main application windows existence.
+        widget.removeWidget(login_window) # Remove the login widget.
+        widget.addWidget(user_window) # Add the users main application window.
+
+########################################################################################################################
+
+# The signup class - currently not unimplented for actual signup.
+class SignUpPage(QDialog): 
+    def __init__(self):
+        super(SignUpPage,self).__init__()
+        loadUi(r'Client\pages\signup_page.ui',self)
+        self.submit_button.clicked.connect(self.signup_function)
+        self.return_button.clicked.connect(self.open_login_page)
+
+    def signup_function(self):
+        username = self.username_line_edit.text()
+        email = self.email_line_edit.text()
+        password = self.password_line_edit.text()
+        confirm_password = self.confirm_password_line_edit.text()
+        print(f"username = {username} | email = {email} | password = {password} | confirm_password = {confirm_password}")
+    
+    def open_login_page(self):
+        widget.removeWidget(signup_window) # Remove the login widget.
+        widget.addWidget(login_window) # Add the users main application window.
+
+########################################################################################################################
 
 # The main application window where users will interact with their priveledge data.
 class UserPage(QDialog):
@@ -87,6 +116,8 @@ class UserPage(QDialog):
         loadUi(r'Client\pages\user_page_test.ui', self) # Load the corresponding ui.
 
 
+########################################################################################################################
+
 # Pre application requirements
 load_dotenv() # Load the environment variables.
 CLIENT.connect((getenv("pub_Ip"), int(getenv("pub_port")))) # Connect to the client
@@ -94,8 +125,9 @@ CLIENT.connect((getenv("pub_Ip"), int(getenv("pub_port")))) # Connect to the cli
 # Application startup requirements and control.
 app=QApplication(sys.argv)
 widget=QtWidgets.QStackedWidget()
-loginwindow = LoginPage() # Define the existance of the login application.
-widget.addWidget(loginwindow)
+login_window = LoginPage() # Define the existance of the login application.
+signup_window = SignUpPage()
+widget.addWidget(login_window)
 widget.resize(1080,720)
 widget.setMaximumWidth(1920)
 widget.setMaximumHeight(1080)
